21125671848ed0aea9f68251aee1ee2bd102a164
yiwenfeng <1918346819@qq.com>
2025-04-25 15:23:47 +0800

    初始化项目

---
+++ src/main/java/com/ywf/ywfpicturebackend/YwfPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ywf.ywfpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwfPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YwfPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ywf/ywfpicturebackend/YwfPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywf.ywfpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwfPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
