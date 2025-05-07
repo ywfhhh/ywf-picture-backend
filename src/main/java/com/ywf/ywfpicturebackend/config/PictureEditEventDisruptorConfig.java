e15c764a9bbcac942f7f63e62c0d4fe4d9dd6a83
yiwenfeng <1918346819@qq.com>
2025-05-08 00:31:21 +0800

    1.websocket实现多人在线实时编辑图片 2.disruptor高性能消息队列实现异步并发处理交互事件

---
+++ src/main/java/com/ywf/ywfpicturebackend/config/PictureEditEventDisruptorConfig.java
@@ -0,0 +1,37 @@
+package com.ywf.ywfpicturebackend.config;
+
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.ywf.ywfpicturebackend.websocket.disruptor.PictureEditEvent;
+import com.ywf.ywfpicturebackend.websocket.disruptor.PictureEditEventWorkHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * websocket的分发过程和处理过程为同一线程,disrupt通过引入环状队列实现异步并发处理一个websocket连接上的事件
+ */
+@Configuration
+public class PictureEditEventDisruptorConfig {
+
+    @Resource
+    private PictureEditEventWorkHandler pictureEditEventWorkHandler;// disruptor无锁队列实现并发处理一个websocket连接上的事件
+
+    @Bean("pictureEditEventDisruptor")
+    public Disruptor<PictureEditEvent> messageModelRingBuffer() {
+        // ringBuffer 的大小
+        int bufferSize = 1024 * 256;
+        Disruptor<PictureEditEvent> disruptor = new Disruptor<>(
+                PictureEditEvent::new,
+                bufferSize,
+                ThreadFactoryBuilder.create().setNamePrefix("pictureEditEventDisruptor").build()
+        );
+        // 设置消费者
+        disruptor.handleEventsWithWorkerPool(pictureEditEventWorkHandler);
+        // 开启 disruptor
+        disruptor.start();
+        return disruptor;
+    }
+}
+
---
+++ src/main/java/com/ywf/ywfpicturebackend/websocket/disruptor/PictureEditEvent.java
@@ -0,0 +1,31 @@
+package com.ywf.ywfpicturebackend.websocket.disruptor;
+
+import com.ywf.ywfpicturebackend.model.entity.User;
+import com.ywf.ywfpicturebackend.websocket.model.PictureEditRequestMessage;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+@Data
+public class PictureEditEvent {
+
+    /**
+     * 消息
+     */
+    private PictureEditRequestMessage pictureEditRequestMessage;
+
+    /**
+     * 当前用户的 session
+     */
+    private WebSocketSession session;
+
+    /**
+     * 当前用户
+     */
+    private User user;
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+}
